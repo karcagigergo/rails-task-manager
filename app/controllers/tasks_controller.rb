b074572f88a7eba7f1939c046361088da94c4ec3
Gergo Karcagi <karcagigergo@gmail.com>
2021-04-17 14:57:17 -0300

    Refactoring v1.0

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,13 @@
 class TasksController < ApplicationController
+    before_action :find_task, only: [:show, :edit, :update, :destroy]
 
     def home; end
 
-    def all
+    def index
         @tasks = Task.all
     end
 
-    def show
-        @task = Task.find(params[:id])
-    end
+    def show; end
 
     def new
         @task = Task.new # needed to instantiate the form_for
@@ -21,22 +20,16 @@ class TasksController < ApplicationController
         redirect_to task_path(@task)
     end
 
-    def edit
-        @task = Task.find(params[:id])
-    end
+    def edit; end
 
     def update
-        @task = Task.find(params[:id])
         @task.update(task_strong_params)
-    
         # no need for app/views/restaurants/update.html.erb
         redirect_to task_path(@task)
     end
 
     def destroy
-        @task = Task.find(params[:id])
         @task.destroy
-    
         # no need for app/views/restaurants/destroy.html.erb
         redirect_to tasks_path
     end
@@ -46,4 +39,9 @@ class TasksController < ApplicationController
     def task_strong_params
         params.require(:task).permit(:title, :details, :completed)
     end
+
+    def find_task
+        @task = Task.find(params[:id])
+    end
+
 end
