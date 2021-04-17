872cd356bbf8b0c9d13e76b5a6ff355bd3a734d7
Gergo Karcagi <karcagigergo@gmail.com>
2021-04-17 13:08:31 -0300

    Made the CREATE from CRUD

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,4 +9,22 @@ class TasksController < ApplicationController
     def show
         @task = Task.find(params[:id])
     end
+
+    def new
+        @task = Task.new # needed to instantiate the form_for
+    end
+
+    def create
+        @task = Task.new(task_strong_params)
+        @task.save
+    
+        # no need for app/views/restaurants/create.html.erb
+        redirect_to task_path(@task)
+    end
+
+    private
+
+    def task_strong_params
+        params.require(:task).permit(:title, :details, :completed)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'tasks#home'
-  get "tasks", to: "tasks#all", as: :tasks
-  get "tasks/:id", to: "tasks#show", as: :task
+  root to: 'tasks#home' # The root
+  get "tasks", to: "tasks#all", as: :tasks # As a user, I can list tasks READ
+  get "tasks/new", to: "tasks#new" # The `new` route needs to be *before* `show` route. CREATE - step 1.
+  get "tasks/:id", to: "tasks#show", as: :task # As a user, I can view the details of a task READ
+  post "tasks", to: "tasks#create" # CREATE - step 2.
+  get "tasks/:id/edit", to: "tasks#edit" # Update - Step 1, GET the form
+  
 end
