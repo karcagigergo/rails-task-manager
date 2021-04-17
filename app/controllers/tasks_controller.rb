25c28808b21cffa2890e4fb3d9020d5df64f5485
Gergo Karcagi <karcagigergo@gmail.com>
2021-04-17 12:53:41 -0300

    Made the READ from CRUD

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,12 @@
 class TasksController < ApplicationController
 
+    def home; end
+
     def all
         @tasks = Task.all
+    end
+
+    def show
+        @task = Task.find(params[:id])
     end
 end
