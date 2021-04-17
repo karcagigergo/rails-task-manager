726cb6c7575ff4f02e38cbb1dbe6c12e972d6ab2
Gergo Karcagi <karcagigergo@gmail.com>
2021-04-17 14:36:17 -0300

    Minor changes in the CRUD eg. links, buttons for redirection purposes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get "tasks/new", to: "tasks#new", as: :new # The `new` route needs to be *before* `show` route. CREATE - step 1.
   get "tasks/:id", to: "tasks#show", as: :task # As a user, I can view the details of a task READ
   post "tasks", to: "tasks#create" # CREATE - step 2.
-  get "tasks/:id/edit", to: "tasks#edit" # Update - Step 1, GET the form
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit # Update - Step 1, GET the form
   patch "tasks/:id", to: "tasks#update" # Update - Step 2, PATCH the form
   
 end
